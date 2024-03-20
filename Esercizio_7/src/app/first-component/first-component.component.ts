3657666a536c6713d38e0e85e145730d8dcaf629
Tommaso Rossi <rossi.tommaso@iisgalvanimi.edu.it>
2024-03-20 22:20:03 +0000

    ho inserito i bottoni

--- Esercizio_7/src/app/first-component/first-component.component.ts
+++ Esercizio_7/src/app/first-component/first-component.component.ts
@@ -9,4 +9,13 @@ import { Component } from '@angular/core';
 })
 export class FirstComponentComponent {
 
+  clickBottone(link: string){
+    window.open(link);
+  }
+
+  pokemon = [
+
+  {}
+
+  ]
 }
