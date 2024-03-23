9b7fa29349bfad0263ae0112f1b676400445cff9
Tommaso Rossi <rossi.tommaso@iisgalvanimi.edu.it>
2024-03-23 16:48:46 +0000

    ho inserito le informazioni di ogni starter ma non funziona il data binding

--- Esercizio_7/src/app/first-component/first-component.component.ts
+++ Esercizio_7/src/app/first-component/first-component.component.ts
@@ -12,15 +12,16 @@ export class FirstComponentComponent {
 
   clickBottone(link: string){
     window.open(link);
+    
   }
 
   pokemon = [
 
-  {nome:"STARYU", immagine:"https://media.pokemoncentral.it/wiki/5/52/Artwork0120_RFVF.png", colore:"blu"},
+  {nome:"#120 STARYU", colore:"azzuro", tipo:"acqua", peso:"34,5 kg", altezza:"0,8 metri", posizione:"kanto #120, johto #169, hoenn #148, unima #238, kalos #034, alola #223, armatura#098", tassocattura:"225 (39,9%)",},
 
-  {nome:"ARON", immagine:"https://media.pokemoncentral.it/wiki/thumb/9/90/Artwork0304_RZ.png/300px-Artwork0304_RZ.png", colore:"rosso" },
+  {nome:"#304 ARON", colore:"grigio", tipo:"acciaio/roccia", peso:"60,0 kg", altezza:"0,4 metri", posizione:"hoenn #072, unima #166, kalos #099, corona #191", tassocattura:"180 (33,8%)", },
 
-  {nome:"SAWK", immagine:"https://media.pokemoncentral.it/wiki/thumb/a/a9/Artwork0539_NB.png/300px-Artwork0539_NB.png", colore:"nero"},
+  {nome:"#539 SAWK", colore:"oro", tipo:"lotta", peso:"51,0 kg", altezza:"1,4 metri", posizione:"unima #268, kalos #098, galar #249", tassocattura:"45 (11,9%)",},
 
   ]
 }
